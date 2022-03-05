098758ef27514de156477c4e0fdf5d9bdd6fee44
Chirag Bhatia <cbhatia2002@gmail.com>
2022-03-05 11:35:43 +0530

    feat: card component WIP

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -1,7 +1,27 @@
 import React from 'react'
 
+import { useStyles } from './styles'
+import { Grid } from '@material-ui/core'
+
 export const Card = () => {
-  return (
-    <div>Footer</div>
-  )
+    const classes = useStyles()
+
+    return (
+        <div className={classes.card}>
+            <Grid container>
+                <Grid item xs={4} lg={4}>
+                    Hello
+                </Grid>
+                <Grid item xs={4} lg={4}>
+                    Hello
+                </Grid>
+                <Grid item xs={4} lg={4}>
+                    Hello
+                </Grid>
+                <Grid item xs={4} lg={4}>
+                    Hello
+                </Grid>
+            </Grid>
+        </div>
+    )
 }
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
 
+import { Card } from '../../components/card'
+
 export const Home = () => {
   return (
-    <div>Home</div>
+    <>
+      <div>Home</div>
+      <Card />
+    </>
   )
 }
